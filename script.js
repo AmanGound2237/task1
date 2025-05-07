52d8ec1f0ae35d5ab73b4ea5e3e273de1b737c55
Gound Aman Anil <189952355+AmanGound2237@users.noreply.github.com>
2025-05-07 19:51:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Mobile Menu
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  const menu = document.getElementById('mobile-menu');
+  menu.classList.toggle('hidden');
+});
